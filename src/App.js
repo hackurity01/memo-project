5c6327ee59b9199802be91aa931d23aed68ee169
hackurity01 <hackurity01@gmail.com>
2022-01-04 01:01:29 +0900

    7-2. 기본 컴포넌트 추가하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MemoContainer from './components/MemoContainer';
+import SideBar from './components/SideBar';
 
 function App() {
+  const [memos, setMemos] = useState([
+    {
+      title: 'Memo 1',
+      content: 'This is memo 1',
+      createdAt: 1641225302265,
+      updatedAt: 1641225302265,
+    },
+    {
+      title: 'Memo 2',
+      content: 'This is memo 2',
+      createdAt: 1641225309267,
+      updatedAt: 1641225309267,
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar memos={memos} />
+      <MemoContainer />
     </div>
   );
 }
